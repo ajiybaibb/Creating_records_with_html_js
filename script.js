6fab0f60e4ffbad0b8cdf7388babebe1e6d59d2a
Ajiyba İbrahim Bekir Bektaş <83498865+ajiybaibb@users.noreply.github.com>
2022-03-14 18:32:04 +0300

    codes added

---
+++ script.js
@@ -0,0 +1,7 @@
+function kayıt(){
+    var adlar=new Array("ali","mehmet","hüseyin","ahmet kaya");
+    var şifreler=new Array("12","123","1234","13456");
+    adlar.push(kod.value);
+    şifreler.push(sifreler.value);
+document.getElementById("sonuç").innerHTML="kaydınız başarılı oluştu";
+}
